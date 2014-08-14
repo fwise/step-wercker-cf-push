fab626700737d070f33b70dc42941f39779ebeed
John Calabrese <jcalabrese@pivotal.io>
2014-08-14 12:23:02 -0400

    minimalist yaml and new run
    restructuring the run.sh and yaml files
    to be more in line with other project in the
    wercker directory.
    
    removing properties in yml in favor of
    logic in run.sh

--- run.sh
+++ run.sh
@@ -1,67 +1,114 @@
+#!/bin/sh
+set -e
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_API_URL" ]
+then
+    fail 'missing or empty option cloudfoundry_api_url, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME" ]
+then
+    fail 'missing or empty option cloudfoundry_app_name, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_NAME" ]
+then
+    fail 'missing or empty option cloudfoundry_user_name, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS" ]
+then
+    fail 'missing or empty option cloudfoundry_user_pass, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_ORG" ]
+then
+    fail 'missing or empty option cloudfoundry_org, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_SPACE" ]
+then
+    fail 'missing or empty option cloudfoundry_space, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_PUSH_CLOUDFOUNDRY_USE_MANIFEST" ]
+then
+    fail 'missing or empty option cloudfoundry_use_manifest, please check wercker.yml'
+fi
+
 wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v6.3.2/cf-linux-amd64.tgz
 tar -zxvf cf-linux-amd64.tgz
 CF=./cf
-
 ${CF} api ${WERCKER_CF_PUSH_CLOUDFOUNDRY_API_URL}
-
-appname=${WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME}
-username=${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_NAME}
-password=${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS}
-organization=${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS}
-space=${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS}
-${CF} login -u $username -p $password -o $organization -s $space
-
+${CF} login -u ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_NAME} -p ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USER_PASS} -o ${WERCKER_CF_PUSH_CLOUDFOUNDRY_ORG} -s ${WERCKER_CF_PUSH_CLOUDFOUNDRY_SPACE}
 PUSH_CMD=""
 
 if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_USE_MANIFEST} == true ]; then
-  PUSH_CMD="${CF} push ${appname}"
+  PUSH_CMD="${CF} push ${WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME}"
 
 else
-  PUSH_CMD="${CF} push ${appname}"
+  PUSH_CMD="${CF} push ${WERCKER_CF_PUSH_CLOUDFOUNDRY_APP_NAME}"
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_BUILDPACK} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_BUILDPACK} ]; then
     PUSH_CMD="${PUSH_CMD} -b ${WERCKER_CF_PUSH_CLOUDFOUNDRY_BUILDPACK}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_COMMAND} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_COMMAND} ]; then
     PUSH_CMD="${PUSH_CMD} -c ${WERCKER_CF_PUSH_CLOUDFOUNDRY_COMMAND}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_DOMAIN} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_DOMAIN} ]; then
     PUSH_CMD="${PUSH_CMD} -d ${WERCKER_CF_PUSH_CLOUDFOUNDRY_DOMAIN}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NUM_INSTANCES} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NUM_INSTANCES} ]; then
     PUSH_CMD="${PUSH_CMD} -i ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NUM_INSTANCES}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_MEMORY} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_MEMORY} ]; then
     PUSH_CMD="${PUSH_CMD} -m ${WERCKER_CF_PUSH_CLOUDFOUNDRY_MEMORY}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST} ]; then
     PUSH_CMD="${PUSH_CMD} -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_HOST}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_PATH} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_PATH} ]; then
     PUSH_CMD="${PUSH_CMD} -p ${WERCKER_CF_PUSH_CLOUDFOUNDRY_PATH}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_STACK} != "" ]; then
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_STACK} ]; then
     PUSH_CMD="${PUSH_CMD} -s ${WERCKER_CF_PUSH_CLOUDFOUNDRY_STACK}"
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_HOSTNAME} ]; then
-    PUSH_CMD="${PUSH_CMD} --no-hostname"
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_HOSTNAME} ]; then
+    if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_HOSTNAME} == true ]; then
+      PUSH_CMD="${PUSH_CMD} --no-hostname"
+    fi
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_ROUTE} ]; then
-    PUSH_CMD="${PUSH_CMD} --no-route"
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_ROUTE} ]; then
+    if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_ROUTE} == true]; then
+      PUSH_CMD="${PUSH_CMD} --no-route"
+    fi
   fi
 
-  if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_START} ]; then
-    PUSH_CMD="${PUSH_CMD} --no-start"
+  if [ -n ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_START} ]; then
+    if [ ${WERCKER_CF_PUSH_CLOUDFOUNDRY_NO_START} == true ]; then
+      PUSH_CMD="${PUSH_CMD} --no-start"
+    fi
   fi
 fi;
 
-echo ${PUSH_CMD} | sh
+set +e
+push_output=$($PUSH_CMD)
+
+if [[ $? -ne 0 ]];then
+    warning $push_output
+    fail 'push failed';
+
+else
+    success 'finished pushing to cloudfoundry';
+
+fi
+set -e
